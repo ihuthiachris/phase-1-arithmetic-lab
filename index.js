23b0b2a754a00151f862e44a6a8408d794b2296b
Chris Ihuthia Kinuthia <ihuthiachris@gmail.com>
2022-08-21 12:21:32 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
+let num1 = 31
+let num2 = 2
+let multiply = num1 * num2;
 
-// Write your code here
+let random = Math.floor(Math.random() * 10) + 1
+
+let num3 = 32
+let num4 = 8
+let mod = num3 / num4;
+
+let value1 = 5
+let value2 = 10
+let value3 = 15
+let value4 = 20
+
+let max = Math.max(value1, value2, value3, value4);
